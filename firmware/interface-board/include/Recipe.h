c2c8aa638c940261d283462b4c9ba3c3a90b56eb
Connor McMillan <connor@mcmillan.website>
2025-09-06 01:16:15 -0700

    Update Recipe.h
    Updated Recipe.h. I forgot I was working on this before I committed.

--- firmware/interface-board/include/Recipe.h
+++ firmware/interface-board/include/Recipe.h
@@ -6,24 +6,28 @@
 #ifndef RECIPE_H
 #define RECIPE_H
 
+#define MAX_RECIPE_LINES 20
+#define MAX_RECIPE_BENDS 5
+
 class Recipe{
 protected:
   int id = 0; // Id of 0 is invalid
-  int lines[20][5] = {}; // all elements = 0
-  u
+  int lines[MAX_RECIPE_LINES][MAX_RECIPE_BENDS] = {}; // all elements = 0
+  uint8_t linesCompleted = 0;
   int rebarSize = 0;
   char *name = nullptr;
   char *desc = nullptr; //description
   char *note = nullptr;
 
 public:
-  bool getlines(int[20][5]);
-  bool getline(int[5]);
-  bool setlines(int[20][5]);
-  void setline(int x, int y[5]){for (int i=0;i<5;i++) this->lines[x][i]=y[i];};
+  bool getlines(int[MAX_RECIPE_LINES][MAX_RECIPE_BENDS]);
+  bool getline(int[MAX_RECIPE_BENDS]);
+  bool setlines(int[MAX_RECIPE_LINES][MAX_RECIPE_BENDS]);
+  void setline(int x, int y[MAX_RECIPE_BENDS]){for (int i=0;i<MAX_RECIPE_BENDS;i++) this->lines[x][i]=y[i];};
   char* getName(){return name;};
   void setName(char *name){this->name = name;};
   char* getDesc(){return desc;};
+  bool completeLine() {this->linesCompleted++;};
   void setDesc(char *description){this->desc = description;};
 
 };
