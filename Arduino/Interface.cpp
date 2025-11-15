589974e6c92dd92d2b9d4054522d0eebb5ae544f
Connor McMillan <connor@mcmillan.website>
2025-11-15 02:31:36 -0800

    Remove old code
    Remove old Arduino code

--- Arduino/Interface.cpp
+++
@@ -1,8 +0,0 @@
-// John Gilcreast
-// December 15th 2024
-// Interface class implementation file
-// For use with Arduino Driver for Automatic Rebar Bender machine
-
-using namespace std;
-#include "Interface.h"
-#include <iostream>
